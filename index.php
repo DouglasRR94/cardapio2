8997e4dd4832268ba62386292864fe6ded85ecf4
DouglasRR94 <douglas.r.ribeiro94@gmail.com>
2018-11-26 10:36:17 -0200

    Arquivo index.php com a estrutura do array

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+  $cardapio_dia = array(
+    array(
+      "titulo" => "Batatas rústicas",
+      "categoria" => "Entrada",
+      "imagem" => "https://pt.petitchef.com/imgupl/recipe/batatas-rusticas-super-saborosas--md-341832p554029.jpg",
+      "descricao" => "Um rústico acompanhamento,aromático e saboroso para acompanhar carnes. Sucesso garantido em sua refeição."
+  ),
+    array(
+      "titulo" => "Caril de frango",
+      "categoria" => "Principal",
+      "imagem" => "https://pt.petitchef.com/imgupl/recipe/caril-de-frango-facil--md-449716p695097.jpg",
+      "descricao" => "Caril de Frango ou Frango de Caril, tradicional da culinária de Goa, Damão e Diu, antes pertencentes ao Estado Português da Índia, o Caril de Frango é mais conhecido como Frango de Caril em Portugal e Moçambique também. Veja a receita PetitChef desta especialidade Indo-Portuguesa."
+
+  ),
+    array(
+      "titulo" => "Doce de Chocolate",
+      "categoria" => "Sobremesa",
+      "imagem" => "https://pt.petitchef.com/imgupl/recipe/sobremesa-facil-de-chocolate--md-47331p58059.jpg",
+      "descricao" => "Fácil e com uma aparÊncia muito bonita e apetitosa! Servida com um sorvete de creme deve fical maravilhosa."
+
+  )
+);
+
+
+print_r($cardapio_dia);
+
+ ?>
